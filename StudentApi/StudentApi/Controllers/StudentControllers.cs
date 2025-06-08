aed9eee11ec2e4be68b57198aa55c0ce492842c3
Spreethy <preethysrinevasan@gmail.com>
2025-06-08 19:51:31 +0530

    first commit

---
+++ StudentApi/StudentApi/Controllers/StudentControllers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using StudentApi.Models;
+
+namespace StudentApi.Controllers
+{
+    [RoutePrefix("api/students")]
+    public class StudentController : ApiController
+    {
+        [HttpGet, Route("")]
+        public IHttpActionResult GetAllStudents()
+        {
+            return Ok(JsonFileHelper.GetStudents());
+        }
+
+        [HttpGet, Route("{id}")]
+        public IHttpActionResult GetStudent(string id)
+        {
+            var students = JsonFileHelper.GetStudents();
+            var student = students.FirstOrDefault(s => s.Id == id);
+            return student == null ? (IHttpActionResult)NotFound() : Ok(student);
+        }
+
+        [HttpPost, Route("")]
+        public IHttpActionResult AddStudent([FromBody] Student student)
+        {
+            if (student == null || string.IsNullOrWhiteSpace(student.Name))
+                return BadRequest("Invalid student data.");
+
+            var students = JsonFileHelper.GetStudents();
+            student.Id = Guid.NewGuid().ToString();
+            students.Add(student);
+            JsonFileHelper.SaveStudents(students);
+            return Ok(student);
+        }
+
+        [HttpPut, Route("{id}")]
+        public IHttpActionResult UpdateStudent(string id, [FromBody] Student updatedStudent)
+        {
+            if (updatedStudent == null || string.IsNullOrWhiteSpace(updatedStudent.Name))
+                return BadRequest("Invalid student data.");
+
+            var students = JsonFileHelper.GetStudents();
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null) return NotFound();
+
+            student.Name = updatedStudent.Name;
+            student.Age = updatedStudent.Age;
+            student.Course = updatedStudent.Course;
+            JsonFileHelper.SaveStudents(students);
+            return Ok(student);
+        }
+
+        [HttpDelete, Route("{id}")]
+        public IHttpActionResult DeleteStudent(string id)
+        {
+            var students = JsonFileHelper.GetStudents();
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null) return NotFound();
+
+            students.Remove(student);
+            JsonFileHelper.SaveStudents(students);
+            return Ok();
+        }
+    }
+}
---
+++ StudentApi/StudentApi/Helpers/JsonFileHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Hosting;
+using StudentApi.Models;
+
+public static class JsonFileHelper
+{
+    private static readonly string filePath = HostingEnvironment.MapPath("~/App_Data/students.json");
+
+    public static List<Student> GetStudents()
+    {
+        if (!File.Exists(filePath))
+        {
+            SaveStudents(new List<Student>());
+        }
+
+        var jsonData = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<List<Student>>(jsonData) ?? new List<Student>();
+    }
+
+    public static void SaveStudents(List<Student> students)
+    {
+        var jsonData = JsonConvert.SerializeObject(students, Formatting.Indented);
+        File.WriteAllText(filePath, jsonData);
+    }
+}
